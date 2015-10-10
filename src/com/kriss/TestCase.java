486d8d0829d83a37e9ca46fb9444b701cf3aaf24
kriss <kriss@192.168.126.1>
2015-10-10 17:14:50 +0800

    riqi

--- src/com/kriss/TestCase.java
+++ src/com/kriss/TestCase.java
@@ -1,10 +1,13 @@
 package com.kriss;
 
+import java.util.Date;
+
 public class TestCase {
 
 	public static void main(String[] args) {
-	 System.out.print("First Test");
-
+	   System.out.print("First Test");
+       Date  date = new Date();
+       
 	}
 
 }
